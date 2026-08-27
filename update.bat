f41361815125103d7cd49a6669daf970bc804e38
NajibMauthoor <realtreforis@gmail.com>
2026-08-26 22:15:18 -0400

    Auto Updates the app with scores each hour

--- update.bat
+++ update.bat
@@ -2,4 +2,7 @@ cd /d C:\Users\Admin\Documents\Projects\Football-Score-Predictor
 python fetch.py
 python pipeline.py
 python fetch.py
+git add artifacts
+git commit -m "Daily update"
+git push
 pause
